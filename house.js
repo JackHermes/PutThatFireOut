3cfa1b17dcd947a5a6323b0b911e1719c8e34ac5
Jordan Scholten <schojo13@gmail.com>
2018-04-12 00:00:33 -0700

    Add four-directional movement to resident

--- house.js
+++ house.js
@@ -3,6 +3,11 @@ let ctx = canvas.getContext("2d");
 let residentX = canvas.width/2;
 let residentY = canvas.height-30;
 
+let upPressed = false;
+let downPressed = false;
+let rightPressed = false;
+let leftPressed = false;
+
 function drawResident() {
   ctx.beginPath();
   ctx.arc(residentX, residentY, 10, 0, Math.PI*2);
@@ -30,6 +35,53 @@ function drawExtinguisherCone() {
   ctx.fill();
 }
 
-drawResident();
-drawFire();
-drawExtinguisherCone();
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+// Key codes:
+// left-37; up-38; right-39; down-40
+function keyDownHandler(e) {
+  if(e.keyCode === 40) {
+    downPressed = true;
+  } else if(e.keyCode === 39) {
+    rightPressed = true;
+  } else if(e.keyCode === 38) {
+    upPressed = true;
+  } else if(e.keyCode === 37) {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if(e.keyCode === 40) {
+    downPressed = false;
+  } else if(e.keyCode === 39) {
+    rightPressed = false;
+  } else if(e.keyCode === 38) {
+    upPressed = false;
+  } else if(e.keyCode === 37) {
+    leftPressed = false;
+  }
+}
+
+function move() {
+  if(downPressed && residentY < canvas.height) {
+    residentY += 7;
+  } else if(rightPressed && residentX < canvas.width) {
+    residentX += 7;
+  } else if(upPressed && residentY > 0) {
+    residentY -= 7;
+  } else if(leftPressed && residentX > 0) {
+    residentX -= 7;
+  }
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawResident();
+  drawFire();
+  drawExtinguisherCone();
+  move();
+}
+
+setInterval(draw, 30)
